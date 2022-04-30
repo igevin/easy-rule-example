12da0464c7f0f8ce54381f4fe90a91672e775589
gevin <flyhigher139@gmail.com>
2022-04-30 20:47:18 +0800

    init

---
+++ src/main/java/com/example/easyruleexample/EasyRuleExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.easyruleexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyRuleExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyRuleExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/easyruleexample/EasyRuleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easyruleexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyRuleExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
